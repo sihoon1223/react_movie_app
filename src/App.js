12dca32a26ac445d9a59ced791fe76de74c4d8f6
unknown <sihoon1223@naver.com>
2020-03-16 13:38:25 +0900

    navigation 추가

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React, {Component} from 'react';
 import {HashRouter, Route} from "react-router-dom";
 import About from "./routes/About"
 import Home from "./routes/Home"
+import Navigation from "./components/Navigation"
 
 function App() {
     return (
         <HashRouter>
+            {/*link는 무조건 router 안에*/}
+            <Navigation/>
             <Route path="/" exact={true} component={Home}/>
             <Route path="/about" component={About}/>
         </HashRouter>
+
     );
 }
 
